1274009fd73f058f5a02ebfc50c2e2792d241948
Артём Сергеев <persibus@gmail.com>
2022-12-16 15:07:14 +0300

    elasticsearch: add php client

--- bootstrap/SetUp.php
+++ bootstrap/SetUp.php
@@ -11,5 +11,14 @@ class SetUp implements BootstrapInterface
     public function bootstrap($app): void
     {
         $container = \Yii::$container;
+
+        $container->setSingleton(\Elastic\Elasticsearch\Client::class, function () {
+            $hosts = explode(',', env('ES_HOSTS', ''));
+            $builder = \Elastic\Elasticsearch\ClientBuilder::create();
+            if ($hosts) {
+                $builder->setHosts($hosts);
+            }
+            return $builder->build();
+        });
     }
 }
